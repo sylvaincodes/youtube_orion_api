511428e30e5d9c5f72bb6227e1d12371a1492b43
sylvaincodes <adandesylvain2010@gmail.com>
2024-12-25 18:57:51 +0100

    Handle preflight requests (OPTIONS) Issue

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,23 @@ import { clerkClient, clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export default clerkMiddleware(async (auth, req) => {
+  // Handle preflight requests (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Max-Age": "86400", // 24 hours cache
+      },
+    });
+  }
+
   //get frontend authentication token from request
   const { isSignedIn } = await clerkClient.authenticateRequest(req);
+
+  const response = NextResponse.next();
 
   //protect user api
   if (req.nextUrl.pathname.startsWith("/api/user")) {
@@ -26,33 +41,24 @@ export default clerkMiddleware(async (auth, req) => {
     if ((await auth()).sessionClaims?.metadata.role !== "admin") {
       return Response.json(
         {
-          message: "You are not authorized as admin",
+          message: "Forbidden!",
         },
         {
-          status: 401,
+          status: 403,
         }
       );
     }
   }
 
   // else you can continue
-  const response = NextResponse.next();
-
-  // Add CORS headers Second option not needed can be deleted
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
   return response;
 });
 
-// Configure the middleware to run only on API routes
 export const config = {
-  matcher: "/api/:path*",
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
