c4e065b1c678311fcdb93c022d00ff7825d4498e
sylvaincodes <adandesylvain2010@gmail.com>
2024-12-25 16:54:54 +0100

    Solve CORS Issues in next.config.mjs and Add CORS to middleware (Optional)

--- middleware.ts
+++ middleware.ts
@@ -38,14 +38,21 @@ export default clerkMiddleware(async (auth, req) => {
   // else you can continue
   const response = NextResponse.next();
 
+  // Add CORS headers Second option not needed can be deleted
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
   return response;
 });
 
+// Configure the middleware to run only on API routes
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: "/api/:path*",
 };
--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,17 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path",
+        // Apply these headers to all routes
+        source: "/api/:path*",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "*", //allow every origin
-            // value: "http://localhost:3000" //allow only one origin
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
-
           {
             key: "Access-Control-Allow-Headers",
             value: "Content-Type, Authorization",
